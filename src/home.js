f1721b23414fb5a29f659ef92b86c3dac8f3b56f
yiechernchuen <yccgithub@gmail.com>
2023-08-01 03:00:20 +0800

    Optimise images

--- src/home.js
+++ src/home.js
@@ -1,5 +1,6 @@
 import { homeButton, homeContent } from './webpage';
-import Icon from './images/logo.png';
+import Icon from './images/logo.jpg';
+import IconWebp from './images/logo.webp';
 
 homeButton.textContent = 'Home';
 
@@ -8,9 +9,14 @@ paragraph.classList.add('introduction');
 homeContent.appendChild(paragraph);
 paragraph.textContent =
     'Welcome to Flavorful Grind, where taste and quality collide! We are passionate about crafting delectable burgers that satisfy your cravings and ignite your taste buds. Our vibrant and inviting ambiance, coupled with exceptional service, creates an unforgettable dining experience. Join us and embark on a culinary journey filled with mouthwatering flavors that will leave you wanting more!';
-
+const picture = document.createElement('picture');
+const sourceWebp = document.createElement('source');
+const sourceJpg = document.createElement('source');
+sourceWebp.setAttribute('srcset', IconWebp);
+sourceJpg.setAttribute('srcset', Icon);
 const logo = new Image();
 logo.src = Icon;
 logo.alt = 'Logo';
 logo.classList.add('logo');
-homeContent.appendChild(logo);
+picture.append(sourceWebp, sourceJpg, logo);
+homeContent.appendChild(picture);
